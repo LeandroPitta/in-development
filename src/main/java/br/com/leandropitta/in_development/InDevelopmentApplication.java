52718cd66bfc29bc7785746c8bf9c72833ca4ab6
Leandro Pitta <lspitta@gmail.com>
2024-12-26 13:44:01 -0300

    initialize Spring Boot project with basic structure and configuration

---
+++ src/main/java/br/com/leandropitta/in_development/InDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package br.com.leandropitta.in_development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InDevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leandropitta/in_development/InDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandropitta.in_development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
